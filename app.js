ed1e937b7146efb739e80f95943374e9839d6ef6
Redcheek1 <106299921+Redcheek1@users.noreply.github.com>
2022-06-02 11:19:53 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ let app = express();
 
 let port = 8080;
 
-app.get( '/', ( req, res ) => res.send( 'Hellp, Node.js!!!' ) );
+app.get( '/', ( req, res ) => res.send( 'Hello, Node.js!!! - GET method' ) );
+
+app.post( '/', ( req, res ) => res.send( 'Hello, Node.js!!! - POST method' ) );
 
 app.listen( port, () => console.log( `Server is running... [ ${port} ]` ) );
 
